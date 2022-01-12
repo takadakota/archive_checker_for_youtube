8481ba2c83d43a88b4d5f956f32498eb24f6cc48
TakadaKota <>
2022-01-13 05:49:38 +0900

    update: コメント総数も出す、replace関数間違えてたので修正

--- kirinuki_API.py
+++ kirinuki_API.py
@@ -4,7 +4,7 @@ import datetime
 
 # is_alive()でコメント欄を動的な状態に保っていったん欲しいデータをlistに入れる、それを処理で加工する
 # force_replay=True は対象URLがアーカイブの場合つけるとよい
-livechat = pytchat.create(video_id = "nNKg8nHG9Fc", force_replay=True)
+livechat = pytchat.create(video_id = "F4SKZ6bR7f0", force_replay=True)
 elapsedTime_list = []
 comment_list = []
 while livechat.is_alive():
@@ -16,7 +16,7 @@ while livechat.is_alive():
     time.sleep(0.1)
 # 時刻だけのdatetime型を定義、動画の終了時刻は手入力が必要。また日付部分は必須なので適当に22年元旦とする、この日付はこのあとの処理で見ないようにする
 start_time = datetime.datetime(2022, 1, 1, 0, 0, 0)
-end_time = datetime.datetime(2022, 1, 1, 2, 54, 0)
+end_time = datetime.datetime(2022, 1, 1, 2, 4, 0)
 check_time_front = start_time
 check_time_back = check_time_front + datetime.timedelta(minutes = 1)
 first_flg = True
@@ -36,7 +36,7 @@ while check_time_front < end_time:
         elif len(elapsedTime) == 5:
             # 待機枠のコメントは配信開始直前のものは-0:09みたいな感じで保存されてるぽいので、うまく処理する必要がある。
             if "-" in elapsedTime:
-                elapsedTime.replace("-", "0")
+                elapsedTime = elapsedTime.replace("-", "0")
                 chat_time_repair = "00:" + elapsedTime
                 chat_time = datetime.datetime.strptime(chat_time_repair, '%H:%M:%S')
             else:
@@ -55,3 +55,4 @@ while check_time_front < end_time:
             break
     if comment_count > 0:
         print("動画時間: " + str(check_time_front).split(" ")[1] + "〜" + str(check_time_back).split(" ")[1] + ", コメント数: " + str(comment_count))
+    print("コメント総数: " + str(len(comment_list)))
